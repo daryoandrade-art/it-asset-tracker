3c853cf5b862989a0ab2449b85edb8edf10c23e1
daryoandrade-art <daryoandrade20@hotmail.com>
2026-07-17 13:52:01 -0300

    feat: adiciona entidade asset

---
+++ src/main/java/br/com/daryo/it_asset_tracker/model/Asset.java
@@ -0,0 +1,48 @@
+package br.com.daryo.it_asset_tracker.model;
+
+import br.com.daryo.it_asset_tracker.model.enums.AssetEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "asset")
+public class Asset {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String hostname;
+
+    @Column(name = "serial_number", unique = true, nullable = false)
+    private String serialNumber;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private AssetEnum status;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "site_id")
+    private Site site;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private SiteUser user;
+}
---
+++ src/main/java/br/com/daryo/it_asset_tracker/model/enums/AssetEnum.java
@@ -0,0 +1,13 @@
+package br.com.daryo.it_asset_tracker.model.enums;
+
+public enum AssetEnum {
+    IN_TRANSIT,
+    CONFIGURING,
+    IN_STOCK,
+    IN_USE,
+    RESERVED,
+    UNDER_REPAIR,
+    IN_WARRANTY,
+    STOLEN_LOST,
+    DISPOSED
+}
